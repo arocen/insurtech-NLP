ff0957f6752e610d7c4d0e7612f8e49bf9928f28
arocen <aufger1913@hotmail.com>
2023-11-02 19:22:32 +0800

    update query function and words

--- useWordEmbeddings.py
+++ useWordEmbeddings.py
@@ -13,15 +13,19 @@ vector = model.wv['保险']
 print(vector)
 
 # get the most simliar words
-sims = model.wv.most_similar('保险科技', topn=10)
-print(sims)
+keywords = ['保险科技', '人工智能', '大数据']
+keywords2 = ['保险科技', '互联网保险', '科技', '大数据', '人工智能', '区块链', '云计算', '无人驾驶汽车', '无人机', '物联网', '车联网', '基因检测', '可穿戴设备', '互联网营销', '互联网理财', '第三方支付', '在线支付', '移动支付', '智能投保', '机器学习', '数字营销', '智能客服', '智能理赔']
 
-sims2 = model.wv.most_similar('人工智能', topn=10)
-print(sims2)
+def get_similar_words(keywords:list[str], topn:int=10):
+    '''对于keywords中每个词语，返回topn个最相似的词'''
+    for word in keywords:
+        sims = model.wv.most_similar(word, topn=10)
+        print(word)
+        print(sims)
+        print('\n')
+    return
 
-sims3 = model.wv.most_similar('大数据', topn=10)
-print(sims3)
-
+get_similar_words(keywords2)
 
 # To-do: think about other ways to figure out keywords about insurtech
 
